738b8db2ba83ba44434088ae7b294388b20fa06d
rajeevjaiswaI <84631589+rajeevjaiswaI@users.noreply.github.com>
2021-05-26 17:11:48 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,75 @@
+# Type conversion
+# 1. int to float
+
+print('1. int to float\n')
+
+a = 10
+print(a, type(a))
+
+b=float(a)
+print (b, type(b))
+print('\n')
+
+
+# 2. int to string
+
+print('2. int to string\n')
+
+a = 10
+print(a, type(a))
+
+b=str(a)
+print(b, type(b))
+print('\n')
+
+# 3. int to boolean
+
+print('3. int to boolean\n')
+
+
+a = 10
+print(a, type(a))
+
+b = bool(a)
+print(b, type(b))
+print('\n')
+
+# 4.float to int
+
+print('4. float to int\n')
+
+
+a = 10.01
+print(a,type(a))
+
+b = int(a)
+print(b, type(b))
+print('\n')
+
+# 5. float to string
+
+print('5.float to string \n')
+
+a = 10.01
+print(a, type(a))
+
+b = str(a)
+print(b, type(b))
+print('\n')
+
+# 6. float to boolean
+
+print('6.float to boolean \n')
+
+a = 10.01
+print(a, type(a))
+
+b = bool(a)
+print(b, type(b))
+print('\n')
+
+
+
+# String Formating
+
+input('What is your name?')
